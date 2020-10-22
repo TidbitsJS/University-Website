87eae17e0ea6331ce9b8fc8aaf4dd6114ab5293a
Sujata Gunale <tidbitsjs@gmail.com>
2020-10-22 10:39:47 +0530

    about container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./container/header/Header";
+import About from "./container/about/About";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
     </div>
   );
 }
---
+++ src/container/about/About.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import college from "../../images/College Logo.png";
+import "./about.css";
+
+class About extends Component {
+  render() {
+    return (
+      <section className="about py-5" id="about">
+        <Container>
+          <Row>
+            <Col xs={10} lg={8} md={6} className="mx-auto my-5">
+              <h1 className="text-capitalize">
+                about the <br />
+                <strong className="banner-title">University</strong>
+              </h1>
+              <p className="my-4 text-muted w-75 about-text">
+                Sheikh-Ul-Alam Memorial Degree College, Budgam is the first
+                institution of its kind in the district Budgam. The college
+                while upholding the guiding mantra of famous Rishi
+                Sheikh-Ul-Alam (RA) endeavours to bring about social, moral and
+                spiritual transformation of the social set up by bringing it
+                closer to the currents of prevalent knowledge.
+              </p>
+              <a
+                href="#more"
+                className="btn btn-outline-primary text-uppercase"
+              >
+                learn more{" "}
+                <span>
+                  <i
+                    style={{ marginLeft: 10 }}
+                    className="fas fa-arrow-right"
+                  ></i>
+                </span>
+              </a>
+            </Col>
+            <Col
+              xs={10}
+              lg={4}
+              md={6}
+              className="mx-auto my-5 align-self-center"
+            >
+              <div className="about-img_container">
+                <Image src={college} fluid />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    );
+  }
+}
+
+export default About;
