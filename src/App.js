1343a88629da88126c4d00524a75851973d0da6f
Sujata Gunale <tidbitsjs@gmail.com>
2020-10-22 13:07:29 +0530

    video embed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./container/header/Header";
 import About from "./container/about/About";
+import VideoPage from "./container/videoSection/VideoPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <About />
+      <VideoPage />
     </div>
   );
 }
---
+++ src/container/videoSection/VideoPage.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import bookvideo from "../../videos/Book.mp4";
+import "./video.css";
+
+export class VideoPage extends Component {
+  render() {
+    return (
+      <section>
+        <Container fluid className="video-container">
+          <Row className="video-row">
+            <Col xs={10} className="mx-auto video-col">
+              <video autoPlay loop muted>
+                <source src={bookvideo} type="video/mp4"></source>
+              </video>
+              <div className="video-text">
+                <p>Trusted by 10,000+ students</p>
+                <Button variant="danger">Register</Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    );
+  }
+}
+
+export default VideoPage;
