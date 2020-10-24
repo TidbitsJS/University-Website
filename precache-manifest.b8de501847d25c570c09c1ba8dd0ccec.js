219e7deaf9f1b97557de0dda95201fb59731697b
Sujata Gunale <tidbitsjs@gmail.com>
2020-10-24 18:20:03 +0530

    Updates

---
+++ precache-manifest.b8de501847d25c570c09c1ba8dd0ccec.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c3dd704405b77638a68dbf242e30c3",
+    "url": "/University-Website/index.html"
+  },
+  {
+    "revision": "58dd43103f4006d578fc",
+    "url": "/University-Website/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "cc03bf54fd1ca4521bb4",
+    "url": "/University-Website/static/css/main.b2d89330.chunk.css"
+  },
+  {
+    "revision": "58dd43103f4006d578fc",
+    "url": "/University-Website/static/js/2.1c29c1ac.chunk.js"
+  },
+  {
+    "revision": "09ddb253e7cd5a5d281df67aa70a6036",
+    "url": "/University-Website/static/js/2.1c29c1ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc03bf54fd1ca4521bb4",
+    "url": "/University-Website/static/js/main.28a26df8.chunk.js"
+  },
+  {
+    "revision": "28b9cd248211f8ca01cc",
+    "url": "/University-Website/static/js/runtime-main.81dace9e.js"
+  },
+  {
+    "revision": "7c6312efb180c288ff20552bff3499b7",
+    "url": "/University-Website/static/media/Book.7c6312ef.mp4"
+  },
+  {
+    "revision": "f586ab57568725d4f58ff5cb609e6771",
+    "url": "/University-Website/static/media/College Logo.f586ab57.png"
+  },
+  {
+    "revision": "fce87d3becf9cbfa6032b83e2fc68fc1",
+    "url": "/University-Website/static/media/clogo.fce87d3b.png"
+  },
+  {
+    "revision": "acdcb857adf691c097f5aa6e0d243791",
+    "url": "/University-Website/static/media/course01.acdcb857.jpeg"
+  },
+  {
+    "revision": "b220ebb8c42ac1231b0a9d3b1b9a50ea",
+    "url": "/University-Website/static/media/slide-01.b220ebb8.jpg"
+  },
+  {
+    "revision": "2a4a603ff04670253b05875f9f58b4f7",
+    "url": "/University-Website/static/media/slide-02.2a4a603f.jpg"
+  },
+  {
+    "revision": "f792cfb7223f55b9aea361d3c69d8f5f",
+    "url": "/University-Website/static/media/slide-03.f792cfb7.jpg"
+  }
+]);
