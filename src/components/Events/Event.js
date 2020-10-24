54aefeb68844b3b8bd973a425f81f6004db7ddce
Sujata Gunale <tidbitsjs@gmail.com>
2020-10-24 13:27:26 +0530

    more evetns

--- src/components/Events/Event.js
+++ src/components/Events/Event.js
@@ -4,6 +4,55 @@ import "./event.css";
 
 export class Event extends Component {
   render() {
+    const displayEvent = ["event01", "event02", "event03"].map(
+      (item, index) => (
+        <Col xs={12} key={index + item} className="mt-5">
+          <article className="blog-card">
+            <div className="blog-card__background">
+              <div className="card__background--wrapper">
+                <div
+                  className="card__background--main"
+                  style={{
+                    backgroundImage:
+                      "url(http://demo.yolotheme.com/html/motor/images/demo/demo_131.jpg)",
+                  }}
+                >
+                  <div className="card__background--layer"></div>
+                </div>
+              </div>
+            </div>
+            <div className="blog-card__head">
+              <span className="date__box">
+                <span className="date__day">11</span>
+                <span className="date__month">JAN</span>
+              </span>
+            </div>
+            <div className="blog-card__info">
+              <h5>HARVICK GETS WHAT HE NEEDS, JOHNSON AMONG THOSE</h5>
+              <p>
+                <a href="#icon1" className="icon-link mr-3">
+                  <i className="fas fa-user"></i> Tony Jahson
+                </a>
+                <a href="#icon2" className="icon-link">
+                  <i className="fas fa-comments"></i> 150
+                </a>
+              </p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Doloremque vero libero voluptatibus earum? Alias dignissimos quo
+                cum, nulla esse facere atque, blanditiis doloribus at sunt quas,
+                repellendus vel? Et, hic!
+              </p>
+              <a href="#icon3" className="btn btn--with-icon">
+                <i className="btn-icon fas fa-long-arrow-alt-right"></i>READ
+                MORE
+              </a>
+            </div>
+          </article>
+        </Col>
+      )
+    );
+
     return (
       <section>
         <Container
@@ -21,52 +70,7 @@ export class Event extends Component {
           </Row>
         </Container>
         <Container className="mt-5">
-          <Row>
-            <Col xs={12}>
-              <article className="blog-card">
-                <div className="blog-card__background">
-                  <div className="card__background--wrapper">
-                    <div
-                      className="card__background--main"
-                      style={{
-                        backgroundImage:
-                          "url(http://demo.yolotheme.com/html/motor/images/demo/demo_131.jpg)",
-                      }}
-                    >
-                      <div className="card__background--layer"></div>
-                    </div>
-                  </div>
-                </div>
-                <div className="blog-card__head">
-                  <span className="date__box">
-                    <span className="date__day">11</span>
-                    <span className="date__month">JAN</span>
-                  </span>
-                </div>
-                <div className="blog-card__info">
-                  <h5>HARVICK GETS WHAT HE NEEDS, JOHNSON AMONG THOSE</h5>
-                  <p>
-                    <a href="#icon1" className="icon-link mr-3">
-                      <i className="fas fa-user"></i> Tony Jahson
-                    </a>
-                    <a href="#icon2" className="icon-link">
-                      <i className="fas fa-comments"></i> 150
-                    </a>
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Doloremque vero libero voluptatibus earum? Alias dignissimos
-                    quo cum, nulla esse facere atque, blanditiis doloribus at
-                    sunt quas, repellendus vel? Et, hic!
-                  </p>
-                  <a href="#icon3" className="btn btn--with-icon">
-                    <i className="btn-icon fas fa-long-arrow-alt-right"></i>READ
-                    MORE
-                  </a>
-                </div>
-              </article>
-            </Col>
-          </Row>
+          <Row>{displayEvent}</Row>
         </Container>
       </section>
     );
